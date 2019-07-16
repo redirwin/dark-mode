1eefcd29d8997c74a21e2b0b224c157a9f9c4108
Dave Irwin <david@redirwin.com>
2019-07-16 12:12:59 -0700

    useLocalStorage & setValue functions set up.

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,7 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
+export default function useLocalStorage(key, initialValue) {
+  const [storedValue, updateStoredValue] = useState(() => {
+    const item = localStorage.get(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
 
-export default function useLocalStorage() {
+  const setValue = value => {
+    setStoredValue(value);
+    localStorage.setItem(key, JSON.stringify(value));
+  };
 
-    return(<div></div>)
+  return [storedValue, setValue];
 }
